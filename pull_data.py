921fb2ae6561458e6a24c6f08168c3fa06794d55
yosef gao <1033131660@qq.com>
2016-03-08 14:17:36 +0800

    fix bugs

--- pull_data.py
+++ pull_data.py
@@ -2,8 +2,9 @@ import urllib
 import json
 import time
 from sae.storage import Bucket
+from StringIO import StringIO
 
-ISOTIMEFORMAT='%Y/%m/%d %X'
+ISOTIMEFORMAT='%m/%d/%y %X'
 DATETIMEFORMAT = '%Y_%m_%d'
 TITLE_STR = 'Site,Parameter,Date(LST),Year,Month,Day,Hour,Value,Unit,Duration\r\n'
 WRITE_STR = '%s,PM2.5,%s,%d,%d,%d,%d,%d,ug/m3,%d\r\n'
@@ -23,18 +24,16 @@ def write_aqi_to_file(city):
     time_str = time.strftime(ISOTIMEFORMAT, time.localtime())
     date_str = time.strftime(DATETIMEFORMAT, time.localtime())
     time_s = time.localtime()
-    #if is_file_in_bucket(date_str + '%s' % '.csv'):
-    #    return 'yes'
-    #else:
-    #    return 'no'
-    # create csv file
+    string_file = StringIO()
     filename = date_str + '%s' % '.csv'
     value = get_aqi(city)
-    str_list = []
     if not is_file_in_bucket(filename):
-        str_list.append('Site,Parameter,Date(LST),Year,Month,Day,Hour,Value,Unit,Duration\r\n')
-    str_list.append(WRITE_STR % (city, time_str, time_s.tm_year, time_s.tm_mon, time_s.tm_mday, time_s.tm_hour, value, DURATION))
-    bucket.put_object(filename, ''.join(str_list))
+        string_file.write('Site,Parameter,Date(LST),Year,Month,Day,Hour,Value,Unit,Duration\r\n')
+    else:
+        data = bucket.get_object_contents(filename)
+        string_file.write(data)
+        string_file.write(WRITE_STR % (city, time_str, time_s.tm_year, time_s.tm_mon, time_s.tm_mday, time_s.tm_hour, value, DURATION))
+    bucket.put_object(filename, string_file.getvalue())
     return bucket.generate_url(filename)
 
 def is_file_in_bucket(filename):
